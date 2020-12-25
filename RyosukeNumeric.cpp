ab96c8f33478c347032783743083205f04794ab6
GodRyosuke <ienvs0828@outlook.jp>
2020-12-25 20:45:09 +0900

    First commit Numeric Ryosuke Pro

---
+++ RyosukeNumeric.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+double innerLeadPi(double xn, int n, int k);
+
+double LeadPi(double x0, int n)
+{
+	return innerLeadPi(x0, n, 0);
+}
+
+double innerLeadPi(double xn, int n, int k)
+{
+	if (k == n) {
+		return xn;
+	}
+	return innerLeadPi(xn - tan(xn), n, k + 1);
+}
+
+int main(int argc, char** argv)
+{
+	std::cout << LeadPi(3.0, 1) << std::endl;
+
+	int N = 20;
+	for (int i = 0; i < N; i++) {
+		std::cout << i << ": " << LeadPi(3.0, i) << std::endl;
+	}
+
+	std::cout << 3 - tan(3) << std::endl;
+
+	return 1;
+}
