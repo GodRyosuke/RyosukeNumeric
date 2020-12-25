7259b2dbe94c27818a7f17fb29a1d70989c68bce
GodRyosuke <ienvs0828@outlook.jp>
2020-12-25 20:48:31 +0900

    add comment

--- RyosukeNumeric.cpp
+++ RyosukeNumeric.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 
+// ニュートン法で円周率を求める(f(x) = sinxを使用)
 double innerLeadPi(double xn, int n, int k);
 
 double LeadPi(double x0, int n)
@@ -20,6 +21,8 @@ int main(int argc, char** argv)
 {
 	std::cout << LeadPi(3.0, 1) << std::endl;
 
+	// Nの回数だけニュートン法を実行
+	// 各回数の実行結果を表示
 	int N = 20;
 	for (int i = 0; i < N; i++) {
 		std::cout << i << ": " << LeadPi(3.0, i) << std::endl;
