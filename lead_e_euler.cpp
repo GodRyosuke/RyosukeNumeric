509078b4837cc909fa653c2621f703a0c2d397ad
RyosukeKusumoto <kusuprograming@gmail.com>
2021-12-16 10:47:48 +0900

    add euler

---
+++ lead_e_euler.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+// euler法を用いて常微分方程式を解く
+
+double pow(int a, int b)
+{
+    if (b == 0) {
+        return 1;
+    }
+    if (b > 0) {
+        int ans = 1;
+        for (int i = 0; i < b; i++) {
+            ans *= a;
+        }
+        return (double)ans;
+    }
+    if (b < 0) {
+        double ans = 1;
+        for (int i = 0; i < -b; i++) {
+            ans /= (double)a;
+        }
+        return ans;
+    }
+}
+
+// euler法でネイピア数を導出
+double lead_e(int n, double yn, double h)
+{
+    std::cout << yn << std::endl;
+    // 刻み幅hより、x=1となるnの値は、nh=1より、n = 1/h
+    if (n * h > 1) { // e^xでは、x = 1のときy=eとなるから。
+        return yn;
+    }
+    lead_e(n + 1, (1 + h) * yn, h);
+}
+
+int main(int argc, char** argv)
+{
+    double h = pow(2, -13);
+    std::cout << "h: " << h << std::endl;
+    int max_N = 100;
+    lead_e(1, 1, h);
+    return 0;
+}
